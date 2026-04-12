6624d71cbb5ab2866cdeda35897be8f1d7732323
ShilpaPal109 <palshilpa281@gmail.com>
2026-04-12 18:55:06 +0530

    Add files via upload
    A write to code in CRUD operation

---
+++ HomeController.cs
@@ -0,0 +1,43 @@
+﻿using Crud_Operation.Models;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Crud_Operation.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+
+        [HttpGet]
+        public ActionResult SignUp()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult SignUp(PinCodeMaster pcm)
+        {
+            ViewBag.Message = pcm.Insert();
+            return View();
+        }
+        public ActionResult Show()
+        {
+            PinCodeMaster pcm = new PinCodeMaster();
+            List<PinCodeMaster> lst=pcm.ShowAll();
+            return View(lst);
+        }
+        public ActionResult Delete(int Pincode)
+        {
+            PinCodeMaster pcm=new PinCodeMaster();
+            TempData["Message"] = pcm.Delete(Pincode);
+            return  RedirectToAction("Show");
+        }
+        public ActionResult Edit()
+        {
+            return View();
+        }
+    }
+}
